c13acdc8bf376c31796925934ba04ea6ea3e3413
bhavik <bhavik11111991@gmail.com>
2023-01-02 20:57:31 +0530

    updated code

--- Example3.java
+++ Example3.java
@@ -5,20 +5,18 @@ class Example3{
 		int a = 10;  
 		int b = 20;   
 		//prefix 
-		a = ++a; // =>a+1 => a = aisgn value => 10+1 , a=11
-		b = ++b; // =>21
+		a = ++a; // => a+1 => a = aisgn value => 10+1 , a=11
+		b = ++b; // => 21
 		System.out.println("++a => "+a);  
-		System.out.println("++b => "+b);  
+		System.out.println("++b => "+b);
 		
 		//postfix
 		int x = 10;  
 		int y = 20;   
 		
-		x = x++; // => a = aisgn value => a+1  
+		x = x++; // => x = aisgn value => x+1
 		y = y++; 
 		System.out.println("x++ => "+x);  
 		System.out.println("y++ => "+y);
-		
-			
 	}
 }
