cb4b445aef3d3387439d2c877eedab57465cd940
bhavik <bhavik11111991@gmail.com>
2023-01-09 20:27:38 +0530

    logical && ,|| and Bitwise &,| ternary ? :

---
+++ Example6.java
@@ -0,0 +1,39 @@
+class Example6{
+	public static void main(String args[]){
+		int a=25;  
+		int b=5;  
+		int c=20;  
+		
+		System.out.println(a<c&&a<b);//false && true = false  
+		System.out.println(a<c&b<c); //false & true = false
+		
+		System.out.println(a<c||a<b);//false && true = true  
+		System.out.println(a<c|a<b);//false && true = true
+		
+		System.out.println("================ Ternary Operator ============");
+		/*
+		(condition)? true:false;
+		*/
+		String ans = (a%2 == 0)? "even":"odd";
+		System.out.println("number is "+ ans);
+	}
+}
+/*
+0 - true
+1 - false
+AND 
+0 1  & (&&)
+============
+0 0  0
+0 1  1
+1 1  1
+1 0  1 
+
+OR
+0 1  | (||)
+============
+0 0  0
+0 1  0
+1 0  0
+1 1  1 
+*/
