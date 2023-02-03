e5ff76c15b5ac3396ba1a595f8115a8d4dea599a
bhavik <bhavik11111991@gmail.com>
2023-02-03 20:42:46 +0530

    nested for loops and patterns example

---
+++ Example11.java
@@ -0,0 +1,45 @@
+class Example11{
+	public static void main(String args[]){
+
+		/*
+		==> nested for loop
+		for(initialization; condition; increment/decrement){
+		//statement or code to be executed
+
+			for(initialization; condition; increment/decrement){
+			//statement or code to be executed
+
+			}
+		}
+		*/
+
+		 /*
+		 R C 0 1
+		 0  |1 2|
+		 1  |3 4|2x2
+
+		 [0][0] = 1
+		 [0][1] = 2
+		 [1][0] = 3
+		 [1][1]	= 4
+		 */
+		System.out.println("===========Matrix==============");
+		int i = 1;
+		for(int r=0;r<=1;r++){
+			//System.out.println("=================="+r);
+			for(int c=0;c<=1;c++){
+				//System.out.println("----------------"+c);
+				System.out.println( "["+r+"]"+"["+c+"]" +" = "+(i++));
+			}
+		}
+
+		/*
+		1
+		11
+		111
+		1111
+		11111
+
+		*/
+	}
+}
---
+++ Example13.java
@@ -0,0 +1,35 @@
+class Example13{
+	public static void main(String args[]){
+
+		/*
+		1
+		11
+		111
+		1111
+		11111
+
+		1
+		12
+		123
+		1234
+		12345
+
+		*
+		**
+		***
+		****
+		*****
+
+		*/
+		System.out.println("===========Matrix==============");
+		for(int r=1;r<=5;r++){
+			//System.out.println("=================="+r);
+			for(int c=1;c<=r;c++){
+				//System.out.println("----------------"+c);
+				System.out.print(" * ");
+			}
+			System.out.println("");
+		}
+
+	}
+}
